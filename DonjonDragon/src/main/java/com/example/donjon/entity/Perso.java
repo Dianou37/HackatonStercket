0c6cd37855179c33185c4eaa47d0e04133f4c118
hugo.norguet <hugo.norguet@gmail.com>
2019-12-18 16:56:20 +0100

    test perso entity

--- DonjonDragon/src/main/java/com/example/donjon/entity/Perso.java
+++ DonjonDragon/src/main/java/com/example/donjon/entity/Perso.java
@@ -1,5 +1,21 @@
 package com.example.donjon.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Perso {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	private int hp;
+	private int maxHp;
+	
+	
 
 }
