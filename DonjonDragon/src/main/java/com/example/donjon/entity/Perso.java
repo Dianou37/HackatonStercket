861435b157972edae848dc3754c3edfc2e01d7ce
hugo.norguet <hugo.norguet@gmail.com>
2019-12-19 09:21:47 +0100

    finalisation Class Perso

--- DonjonDragon/src/main/java/com/example/donjon/entity/Perso.java
+++ DonjonDragon/src/main/java/com/example/donjon/entity/Perso.java
@@ -15,7 +15,57 @@ public class Perso {
 	private String name;
 	private int hp;
 	private int maxHp;
+	private String image;
+	private int longitude;
+	private int latitude;
 	
+	public String getName() {
+		return name;
+	}
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getHp() {
+		return hp;
+	}
+	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getMaxHp() {
+		return maxHp;
+	}
+	
+	public void setMaxHp(int maxHp) {
+		this.maxHp = maxHp;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
 
+	public int getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+	
+	
 }
