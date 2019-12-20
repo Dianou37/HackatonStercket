c85c808f98ac6b18775f48f24e93004734e81830
hugo.norguet <hugo.norguet@gmail.com>
2019-12-20 11:40:12 +0100

    Branche finis

--- DonjonDragon/src/main/java/com/example/donjon/controller/PersoController.java
+++ DonjonDragon/src/main/java/com/example/donjon/controller/PersoController.java
@@ -1,12 +1,21 @@
 package com.example.donjon.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.donjon.entity.Perso;
 import com.example.donjon.repository.PersoRepository;
@@ -30,5 +39,26 @@ public class PersoController {
 		model.addAttribute("perso", perso);
 		return "perso_form";
 	}
+	
+	@PostMapping("/addPerso") 
+	public String addPerso(@Valid Perso perso, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()) {
+			return "perso_form";
+		}
+		persoRepository.save(perso);
+		return "redirect:/listPerso";
+		
+	}
+	 
+	@RequestMapping("/showPerso/{id}")
+	public String showPerso(@PathVariable long id, Model model) {
+		Optional<Perso> result = persoRepository.findById(id);
+		if(result.isPresent()) {
+			model.addAttribute("perso", result.get());
+			return "showPerso";
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Personnage inexistant");
+	}
+	
 
 }
--- DonjonDragon/src/main/java/com/example/donjon/entity/Perso.java
+++ DonjonDragon/src/main/java/com/example/donjon/entity/Perso.java
@@ -17,21 +17,28 @@ public class Perso {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	public long id;
 	
 	@NotNull
-	@Size(min=2)
+	@Size(min=2,max=35)
 	private String name;
 	private int hp;
-	private int maxHp;
-	private String image;
-	private int longitude;
-	private int latitude;
+	private int vigueur;
+	private int agilite;
+	private int intelligence;
+	private int ruse;
+	private int volonte;
+	private int presence;
 	
 	public Perso() {
 		Random dice = new Random();
-		hp = dice.nextInt(MAX+20);
-		maxHp = 100;
+		hp = dice.nextInt(MAX)+20;
+		vigueur = dice.nextInt(10);
+		agilite = dice.nextInt(10);
+		intelligence = dice.nextInt(10);
+		ruse = dice.nextInt(10);
+		volonte = dice.nextInt(10);
+		presence = dice.nextInt(10);
 	}
 	
 	public String getName() {
@@ -49,37 +56,53 @@ public class Perso {
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
-	
-	public int getMaxHp() {
-		return maxHp;
-	}
-	
-	public void setMaxHp(int maxHp) {
-		this.maxHp = maxHp;
-	}
-	
-	public String getImage() {
-		return image;
-	}
-	
-	public void setImage(String image) {
-		this.image = image;
+
+	public int getVigueur() {
+		return vigueur;
 	}
 
-	public int getLongitude() {
-		return longitude;
+	public void setVigueur(int vigueur) {
+		this.vigueur = vigueur;
 	}
 
-	public void setLongitude(int longitude) {
-		this.longitude = longitude;
+	public int getAgilite() {
+		return agilite;
 	}
 
-	public int getLatitude() {
-		return latitude;
+	public void setAgilite(int agilite) {
+		this.agilite = agilite;
 	}
 
-	public void setLatitude(int latitude) {
-		this.latitude = latitude;
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+
+	public int getRuse() {
+		return ruse;
+	}
+
+	public void setRuse(int ruse) {
+		this.ruse = ruse;
+	}
+
+	public int getVolonte() {
+		return volonte;
+	}
+
+	public void setVolonte(int volonte) {
+		this.volonte = volonte;
+	}
+
+	public int getPresence() {
+		return presence;
+	}
+
+	public void setPresence(int presence) {
+		this.presence = presence;
 	}
 	
 	
