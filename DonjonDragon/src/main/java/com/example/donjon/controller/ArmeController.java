637e5340c8fa5fe6b1c3797e10a8e6e482081bd6
Aglime <mehdi.aglime@gmail.com>
2019-12-20 11:51:08 +0100

    Generer les arme et leur carac

--- DonjonDragon/src/main/java/com/example/donjon/controller/ArmeController.java
+++ DonjonDragon/src/main/java/com/example/donjon/controller/ArmeController.java
@@ -1,16 +1,20 @@
 package com.example.donjon.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.donjon.entity.Arme;
 import com.example.donjon.repository.ArmeRepository;
@@ -42,5 +46,14 @@ public class ArmeController {
 		armeRepository.save(arme);
 		return "redirect:/list";
 	}
-		
+	@RequestMapping("/show/{id}")
+	public String show(@PathVariable long id, Model model) {
+		Optional<Arme> result= armeRepository.findById(id);
+		if(result.isPresent()) {
+			model.addAttribute("arme", result.get());
+			return "show";
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Arme inexistant");
+	
+	}
 }
--- DonjonDragon/src/main/java/com/example/donjon/entity/Arme.java
+++ DonjonDragon/src/main/java/com/example/donjon/entity/Arme.java
@@ -2,12 +2,11 @@ package com.example.donjon.entity;
 
 import java.util.Random;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -23,17 +22,47 @@ public class Arme {
 	@NotNull
 	@Size(min=2, max=30)
 	private String name;
-	
-	
-	private int degat;
 	private String image;
+
 	
+	private int durabilite;
+	@Column(name="prec")
+	private int precision;
+	private int degat;
+	private int portee;
 	
+
 	public Arme() {
 		Random dice = new Random();
 		degat = dice.nextInt(MAX) + 20;
-		
+		durabilite = dice.nextInt(16) + 5;
+		precision = dice.nextInt(16)  +5;
+		portee =  dice.nextInt(10)  +1;
+		image = "https://www.boutique-epees.fr/18418-large_default/officiel-axe-ragnar-lothbrok-serie-vikings.jpg";
 	}
+	
+	public int getDurabilite() {
+		return durabilite;
+	}
+	
+	public int getPrecision() {
+		return precision;
+	}
+	
+	public int getPortee() {
+		return portee;
+	}
+	public void setDurabilite(int durabilite) {
+		this.durabilite = durabilite;
+	}
+	public void setPrecision(int precision) {
+		this.precision = precision;
+	}
+	public void setPortee(int portee) {
+		this.portee = portee;
+	}
+	
+	
 	public long getId() {
 		return id;
 	}
